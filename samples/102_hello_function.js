f82800cfec3c5e1362003e5f0ecfe240a111f830
ashbfc <74180087+ashbfc@users.noreply.github.com>
2020-12-22 10:14:14 +0530

    Update 102_hello_function.js

--- samples/102_hello_function.js
+++ samples/102_hello_function.js
@@ -11,5 +11,18 @@ var hello = edge.func(function () {/*
 
 hello('Node.js', function (error, result) {
 	if (error) throw error;
+	var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, "ccc", port,"ashji")
+})
 	console.log(result);
 });
